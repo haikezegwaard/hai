e4d9f681c899731083b6d26cb61d2343a6e4fb8a
haikezegwaard <h.zegwaard@gmail.com>
2014-07-20 15:07:32 +0200

    added openlayer map and events

--- meteortest/client/meteortest.js
+++ meteortest/client/meteortest.js
@@ -11,21 +11,78 @@ Template.lobby.me = function(){
 }
 
 Template.loginform.events({
-    'click input.loginbutton' : function () {
-        console.log('login/register');
-        var name = $('myname').val();
-        var curid;
+    'click input.loginbutton' : function () {        
+        var name = $('#myname').val();        
         var result = Players.find({name: name});
-	if(result.count() === 0){
+	if(result.count() === 0){        
 	    Players.insert({name: name}, function(err, docsInserted){
-		if(err) return;
-		console.log(docsInserted);
+		if(err) return;		
+		Session.set('current_player_id', docsInserted );
 	    });
 	}else{
-	    Session.set('current_player_id', result.id );
-	}
-	
+	    Session.set('current_player_id', result._id );
+	}	
     }
 });
+
+var map = null;
+
+Template.map.rendered = function(){
+    if(map){
+	return; //don't do this function twice
+    }
+    OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {                
+	defaultHandlerOptions: {
+	    'single': true,
+	    'double': true,
+	    'pixelTolerance': 0,
+	    'stopSingle': true,
+	    'stopDouble': true
+	},
+
+	initialize: function(options) {
+	    this.handlerOptions = OpenLayers.Util.extend(
+		{}, this.defaultHandlerOptions
+	    );
+	    OpenLayers.Control.prototype.initialize.apply(
+		this, arguments
+	    ); 
+	    this.handler = new OpenLayers.Handler.Click(
+		this, {
+		    'click': this.onClick,
+		    'dblclick': this.onDblclick 
+		}, this.handlerOptions
+	    );
+	}, 
+
+	onClick: function(evt) {
+	    var msg = "click " + evt.xy;
+	    console.log(msg);
+	},
+	
+	onDblclick: function(evt) {  
+	    var msg = "dblclick " + evt.xy;
+	    var lonlat = map.getLonLatFromPixel(evt.xy);
+	    var xlonlat = lonlat.transform(new OpenLayers.Projection("EPSG:900913"), new OpenLayers.Projection("EPSG:4326"));
+	    Settlers.insert({player: Session.get('current_player_id'), lat: xlonlat.lat, lon: xlonlat.lon});
+	    console.log(msg + "transformed: "+ xlonlat.lat+","+ xlonlat.lon);
+	}
+	
+    });
+    map = new OpenLayers.Map('map');
+    var mapnik         = new OpenLayers.Layer.OSM();
+    var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
+    var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
+    var position       = new OpenLayers.LonLat(-122.40146, 37.78212).transform( fromProjection, toProjection);
+    var zoom           = 15; 
+ 
+    map.addLayer(mapnik);
+
+    control = new OpenLayers.Control.Click();
+    map.addControl(control);
+    control.activate();
+
+    map.setCenter(position, zoom );
+}
 
 
--- meteortest/server/server.js
+++ meteortest/server/server.js
@@ -1,11 +1,5 @@
 // On server startup, create some players if the database is empty.
 Meteor.startup(function () {  
-  if (Settlers.find().count() === 0) {
-    
-    var players = ["Haike","Auke","Ivan"];
-    var coors = [103,102,132];
-    for (var i = 0; i < players.length; i++)
-      Settlers.insert({player: players[i], coors: coors[i]});
-  }
+  //startup logic
 });
 
